a4b2cb078a2bd42b13df55deeb7f8e1fc60bc8e6
Nero <14k21096@gmail.com>
2021-11-15 14:34:27 +0300

    Make class which execute 3 first tasks

--- src/Basics/Main.java
+++ src/Basics/Main.java
@@ -1,20 +1,14 @@
 package Basics;
 
-import java.util.Scanner;
+
+import linary_programs.Formulas;
 
 public class Main {
 
     public static void main(String[] args) {
-        float a, b, c, z;
-        Scanner console = new Scanner(System.in);
-        System.out.print("Input a: ");
-        a = console.nextFloat();
-        System.out.print("Input b: ");
-        b = console.nextFloat();
-        System.out.print("Input c: ");
-        c = console.nextFloat();
-        z = ((a - 3) * b / 2) + c;
-        System.out.println("Result: " + z);
-        System.out.println();
+        Formulas form = new Formulas();
+        form.thirdFormula();
+        form.secondFormula();
+        form.firstFormula();
     }
 }
